c81ccbc22a276c15ddb382ca8da14ce291308ca9
Travis CI <travis@travis-ci.org>
2019-01-22 14:58:03 +0000

    Deploy code docs to GitHub Pages Travis build: 15
    Commit: f28dbfc7dc4bac1cda90610f62b8edc084d83fc9

--- html/search/functions_0.js
+++ html/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['adafruit_5fmlx90393',['Adafruit_MLX90393',['../class_adafruit___m_l_x90393.html#aa2043af5974425a843af26a498894c40',1,'Adafruit_MLX90393::Adafruit_MLX90393(int32_t sensorID)'],['../class_adafruit___m_l_x90393.html#aa3a3dff7bd4cb4aa47b84c7ee6dc1b47',1,'Adafruit_MLX90393::Adafruit_MLX90393(int32_t sensorID, TwoWire *wireBus)'],['../class_adafruit___m_l_x90393.html#a843f362c54db2cd5b9e45b759a1ab347',1,'Adafruit_MLX90393::Adafruit_MLX90393(int32_t sensorID, int8_t cs)']]]
+  ['adafruit_5fmlx90393',['Adafruit_MLX90393',['../class_adafruit___m_l_x90393.html#a67ed9a2ac3da9d0c35bfd269486f112e',1,'Adafruit_MLX90393']]]
 ];
