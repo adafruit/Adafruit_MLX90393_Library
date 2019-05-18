01ce52d4c909e6bcbe27d9928530cd4a0866c465
Travis CI <travis@travis-ci.org>
2019-05-18 21:50:57 +0000

    Deploy code docs to GitHub Pages Travis build: 21
    Commit: 84bf03970d3a92710188ec14e00aa49fdba04912

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['adafruit_5fmlx90393',['Adafruit_MLX90393',['../class_adafruit___m_l_x90393.html',1,'Adafruit_MLX90393'],['../class_adafruit___m_l_x90393.html#a67ed9a2ac3da9d0c35bfd269486f112e',1,'Adafruit_MLX90393::Adafruit_MLX90393()']]],
-  ['adafruit_20mlx90393_20magnetic_20field_20sensor_20driver_20_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2ecom_2fadafruit_2fadafruit_5fmlx90393_5flibrary_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2ecom_2fadafruit_2fadafruit_5fmlx90393_5flibrary_29',['Adafruit MLX90393 Magnetic Field Sensor Driver  [![Build Status](https://travis-ci.com/adafruit/Adafruit_MLX90393_Library.svg?branch=master)](https://travis-ci.com/adafruit/Adafruit_MLX90393_Library)',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['adafruit_5fmlx90393',['Adafruit_MLX90393',['../class_adafruit___m_l_x90393.html',1,'Adafruit_MLX90393'],['../class_adafruit___m_l_x90393.html#a67ed9a2ac3da9d0c35bfd269486f112e',1,'Adafruit_MLX90393::Adafruit_MLX90393()']]]
 ];
--- html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['adafruit_20mlx90393_20magnetic_20field_20sensor_20driver_20_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2ecom_2fadafruit_2fadafruit_5fmlx90393_5flibrary_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2ecom_2fadafruit_2fadafruit_5fmlx90393_5flibrary_29',['Adafruit MLX90393 Magnetic Field Sensor Driver  [![Build Status](https://travis-ci.com/adafruit/Adafruit_MLX90393_Library.svg?branch=master)](https://travis-ci.com/adafruit/Adafruit_MLX90393_Library)',['../md__r_e_a_d_m_e.html',1,'']]]
-];
