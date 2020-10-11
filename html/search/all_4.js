b6935958d5ee6cd9082ef8f32a4d66cfe2138686
Doxygen CI <travis@travis-ci.org>
2020-10-11 00:00:13 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/all_4.js
+++ html/search/all_4.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['setgain',['setGain',['../class_adafruit___m_l_x90393.html#a2e1319fd05eaa514e33ad4755a01c1b5',1,'Adafruit_MLX90393']]],
-  ['settrigint',['setTrigInt',['../class_adafruit___m_l_x90393.html#adb8bf5493776fa58fd6e4e604d233df5',1,'Adafruit_MLX90393']]]
+  ['readdata',['readData',['../class_adafruit___m_l_x90393.html#a4d06509810afabf30455282196234178',1,'Adafruit_MLX90393']]]
 ];
--- html/search/searchdata.js
+++ html/search/searchdata.js
@@ -1,21 +1,24 @@
 var indexSectionsWithContent =
 {
-  0: "abgrs",
+  0: "abgirs",
   1: "a",
-  2: "abgrs"
+  2: "abgrs",
+  3: "i"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "classes",
-  2: "functions"
+  2: "functions",
+  3: "variables"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Classes",
-  2: "Functions"
+  2: "Functions",
+  3: "Variables"
 };
 
