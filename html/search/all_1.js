5916252d674205b80f09024d86bf3f7f2899bc89
Doxygen CI <travis@travis-ci.org>
2020-10-11 06:13:38 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/all_1.js
+++ html/search/all_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['begin_5fi2c',['begin_I2C',['../class_adafruit___m_l_x90393.html#a720cc5c8e2403f1d38c0ffdbb2e430ca',1,'Adafruit_MLX90393']]]
+  ['begin_5fi2c',['begin_I2C',['../class_adafruit___m_l_x90393.html#a720cc5c8e2403f1d38c0ffdbb2e430ca',1,'Adafruit_MLX90393']]],
+  ['begin_5fspi',['begin_SPI',['../class_adafruit___m_l_x90393.html#a7359ee069443d78e0506c978bad4f2d2',1,'Adafruit_MLX90393']]]
 ];
