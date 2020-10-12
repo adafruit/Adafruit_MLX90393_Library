6a8edfa2572f4bb3bffbb2d71a89987911068b43
Doxygen CI <travis@travis-ci.org>
2020-10-12 21:47:27 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/functions_3.js
+++ html/search/functions_3.js
@@ -1,6 +1,9 @@
 var searchData=
 [
   ['getevent',['getEvent',['../class_adafruit___m_l_x90393.html#af1a1c40a8289e7269fcf4cc6bb446afe',1,'Adafruit_MLX90393']]],
+  ['getfilter',['getFilter',['../class_adafruit___m_l_x90393.html#a6397ee45fa44b481c5cd635c9ac6cc70',1,'Adafruit_MLX90393']]],
   ['getgain',['getGain',['../class_adafruit___m_l_x90393.html#a1946153995e03566a70a965e2a82bfad',1,'Adafruit_MLX90393']]],
+  ['getoversampling',['getOversampling',['../class_adafruit___m_l_x90393.html#af2fdc0d1490cfa1f968fe4c18b2ef4dd',1,'Adafruit_MLX90393']]],
+  ['getresolution',['getResolution',['../class_adafruit___m_l_x90393.html#a9eb593c18d89cc09a6c226756e4c87a8',1,'Adafruit_MLX90393']]],
   ['getsensor',['getSensor',['../class_adafruit___m_l_x90393.html#a7e2fe029c7a6551732412e705a975ae5',1,'Adafruit_MLX90393']]]
 ];
--- html/search/functions_5.js
+++ html/search/functions_5.js
@@ -1,6 +1,9 @@
 var searchData=
 [
+  ['setfilter',['setFilter',['../class_adafruit___m_l_x90393.html#a2eee57b8e331ff1703c2006687ebd59e',1,'Adafruit_MLX90393']]],
   ['setgain',['setGain',['../class_adafruit___m_l_x90393.html#a2e1319fd05eaa514e33ad4755a01c1b5',1,'Adafruit_MLX90393']]],
+  ['setoversampling',['setOversampling',['../class_adafruit___m_l_x90393.html#ab5e4848eaf3fa38d220c4dc10e7d919e',1,'Adafruit_MLX90393']]],
+  ['setresolution',['setResolution',['../class_adafruit___m_l_x90393.html#ace8f84218fe4b1519a4808eedd925f90',1,'Adafruit_MLX90393']]],
   ['settrigint',['setTrigInt',['../class_adafruit___m_l_x90393.html#adb8bf5493776fa58fd6e4e604d233df5',1,'Adafruit_MLX90393']]],
   ['startsinglemeasurement',['startSingleMeasurement',['../class_adafruit___m_l_x90393.html#a7e0d04fa7ef3948028f289a0b53678ee',1,'Adafruit_MLX90393']]]
 ];
